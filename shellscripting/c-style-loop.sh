b6c136df290f6df6dbebf69674025c6f30bd264c
root <root@ip-172-31-24-139.ap-south-1.compute.internal>
2019-03-03 05:34:42 +0000

    additional shell

---
+++ shellscripting/c-style-loop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for (( i=1; i <= 3; i++ ))
+do
+    echo "Your random number $i: $RANDOM"
+done
---
+++ shellscripting/directory_check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+dir=$1
+
+if [ -d $dir ]; then
+echo "Directory exists"
+else
+echo "Directory doesn't exist"
+fi
+
+
